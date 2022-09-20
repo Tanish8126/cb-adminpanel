8dd509ab88c12c43e3acba9c6736c682d84237b7
Tanish8126 <tanishpurwar37@gmail.com>
2022-09-20 14:03:24 +0530

    2nd commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:adminpanel/constants.dart';
+import 'package:adminpanel/controllers/MenuController.dart';
+import 'package:adminpanel/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:adminpanel/controllers/MenuController.dart';
+import 'package:adminpanel/responsive.dart';
+import 'package:adminpanel/screens/dashboard/dashboard_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'components/side_menu.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: context.read<MenuController>().scaffoldKey,
+      drawer: SideMenu(),
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
+            Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: DashboardScreen(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
